42d1b69e7bc7d80e7fa3fe52a14654be2b982f1a
olowo <olowodarey8@gmail.com>
2025-05-31 10:17:10 +0100

    fix build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Hero from "./component/Hero_section";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,4 @@
-// pages/login.tsx
+"use client";
 import { useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
